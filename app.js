8a55b3e5247c5bb532beb2c123090a6d468a38dc
Audrey Kan <audreykan@outlook.com>
2020-05-24 03:43:58 -0700

    mapbox and input slider

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-const Forecast = () => {
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=37.7531&lon=-122.4954&
+exclude=current,minutely,daily&appid=b8de6b317b0fe4c54fe3f5ba6e6f3db5')
+  .then(response => {
+    return response.json();
+  })
+  .then(users => {
+    console.log(users);
+  });
 
-  let [responseObj, setResponseObj] = useState({});
+const layers = [
+  '32-55', '55-60', '60-65', '65-70', '70-75', '75-100'
+];
+const colors = [
+  '#4892C7', '#0568d6', '#a1c85c', '#FFBD3A', '#FF860A', '#E00300',
+];
 
-  function getForecast() {
-    // ClimaCell API fetch
-    fetch("https://climacell-microweather-v1.p.rapidapi.com/weather/forecast/hourly?fields=feels_like%252Cweather_code&unit_system=us&lat=37.7749&lon=122.4194", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "climacell-microweather-v1.p.rapidapi.com",
-        "x-rapidapi-key": "fce13f8b57msh706d0bccf6d5292p1de690jsn62626372fabb"
-      }
-    })
-    .then(response => response.json())
-    .then(response => {
-      setResponseObj(response)
-    })
-    .catch(err => {
-      console.log(err);
-    });
+$(document).ready( function() {
+  var now = new Date();
+  console.log(now);
+  var option = '';
+  for (var i = 0; i <= 24; i++) {
+    now.setHours( now.getHours() + 1);
+    console.log(now);
+    var hha = now.toLocaleString('en-US', { hour: 'numeric', hour12: true });
+    console.log(hha);
+    options += '<option value="'+hha+'" />';
   }
+  document.getElementById('ticks').innerHTML = options;
 }
